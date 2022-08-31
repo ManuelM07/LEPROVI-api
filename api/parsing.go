8397ca2d2585fbd3d044ea202374098aa11a696f
ManuelM07 <83485551+ManuelM07@users.noreply.github.com>
2022-08-31 11:27:00 -0500

    Create parser Else & For

--- api/parsing.go
+++ api/parsing.go
@@ -42,13 +42,17 @@ func startParsing() {
 		if nodes[k].name == "NodeMath" {
 			//fmt.Println(nodes[k].inputs)
 			fmt.Println(mathOperation("body", k)) // para acceder a la clave de un map de varios niveles
-		} else if nodes[k].name == "NodeAssing" {
-			fmt.Println(assing(k, nodes[k].inputs.(map[string]interface{})))
+		} else if nodes[k].name == "NodeAssign" {
+			fmt.Println(assign(k, nodes[k].inputs.(map[string]interface{})))
 		} else if nodes[k].name == "NodePrint" {
 			fmt.Println(print(nodes[k].inputs.(map[string]interface{})))
 			//fmt.Println(findInput(nodes[k].inputs.(map[string]interface{})["input_1"]))
 		} else if nodes[k].name == "NodeIf" {
-			fmt.Println(nodeIf("body", k))
+			fmt.Println(nodeIf(k))
+		} else if nodes[k].name == "NodeElse" {
+			fmt.Println(nodeElse(k))
+		} else if nodes[k].name == "NodeFor" {
+			fmt.Println(nodeFor(k))
 		}
 	}
 
@@ -68,15 +72,15 @@ func mathOperation(option string, idOutput int) string {
 	}
 }
 
-func assing(pos int, inputs map[string]interface{}) string {
+func assign(pos int, inputs map[string]interface{}) string {
 	node1 := findInput(inputs["input_1"])
 	idNode := findNode(node1)
 	varName := nodes[pos].data.(map[string]interface{})["url"]
 	if nodes[idNode].name == "NodeMath" {
 		answer := mathOperation("", idNode)
 		return fmt.Sprintf("%s = %s", varName, answer)
-	} else if nodes[idNode].name == "NodeIf" {
-		answer := nodeIf("", idNode)
+	} else if nodes[idNode].name == "NodeIf" || nodes[idNode].name == "NodeElse" || nodes[idNode].name == "NodeFor" {
+		answer := valueAssigned(idNode) // Para el caso del else, se puede reutilizar la funcion de nodeIf
 		return fmt.Sprintf("\t%s = %s", varName, answer)
 	}
 	return ""
@@ -88,26 +92,38 @@ func print(inputs map[string]interface{}) string {
 	if nodes[idNode].name == "NodeMath" {
 		answer := mathOperation("n", idNode)
 		return fmt.Sprintf("print(%s)", answer)
-	} else if nodes[idNode].name == "NodeAssing" {
+	} else if nodes[idNode].name == "NodeAssign" {
 		varName := nodes[idNode].data.(map[string]interface{})["url"]
 		return fmt.Sprintf("print(%s)", varName)
+	} else if nodes[idNode].name == "NodeFor" {
+		varName := nodes[idNode].data.(map[string]interface{})["url"]
+		return fmt.Sprintf("\tprint(%s)", varName)
 	}
 	return ""
 }
 
-func nodeIf(option string, idNode int) string {
-	if option == "body" {
-		inputs := nodes[idNode].inputs.(map[string]interface{})
-		node1 := findInput(inputs["input_1"])
-		node2 := findInput(inputs["input_2"])
-		node3 := findInput(inputs["input_3"])
-		nodePos1 := findNode(node1)
-		nodePos2 := findNode(node2)
-		nodePos3 := findNode(node3)
-		return fmt.Sprintf("if %s %s %s:\n", typeNode(nodes[nodePos1], nodePos1), typeNode(nodes[nodePos2], nodePos2), typeNode(nodes[nodePos3], nodePos3))
-	} else {
-		return fmt.Sprintf("%v", nodes[idNode].data.(map[string]interface{})["url"])
-	}
+func nodeIf(idNode int) string {
+	inputs := nodes[idNode].inputs.(map[string]interface{})
+	node1 := findInput(inputs["input_1"])
+	node2 := findInput(inputs["input_2"])
+	node3 := findInput(inputs["input_3"])
+	nodePos1 := findNode(node1)
+	nodePos2 := findNode(node2)
+	nodePos3 := findNode(node3)
+	return fmt.Sprintf("if %s %s %s:\n", typeNode(nodes[nodePos1], nodePos1), typeNode(nodes[nodePos2], nodePos2), typeNode(nodes[nodePos3], nodePos3))
+}
+
+func nodeElse(idNode int) string {
+	return "else:\n"
+}
+
+func nodeFor(idNode int) string {
+	inputs := nodes[idNode].inputs.(map[string]interface{})
+	node1 := findInput(inputs["input_1"])
+	node2 := findInput(inputs["input_2"])
+	nodePos1 := findNode(node1)
+	nodePos2 := findNode(node2)
+	return fmt.Sprintf("for i in range(%s, %s):\n", typeNode(nodes[nodePos1], nodePos1), typeNode(nodes[nodePos2], nodePos2))
 }
 
 func comparison(idOutput int) string {
@@ -141,10 +157,17 @@ func findInput(input interface{}) string {
  */
 func typeNode(node Node, posNode int) string {
 	nameNode := fmt.Sprintf("%v", node.name)
-	if nameNode == "NodeNumber" {
+	if nameNode == "NodeNumber" || nameNode == "NodeAssign" {
 		return fmt.Sprintf("%v", node.data.(map[string]interface{})["url"])
 	} else if nameNode == "NodeComOp" {
 		return comparison(posNode)
 	}
 	return ""
+}
+
+/**
+* Esta función se encarga de retornar el valor a ser asignado en la funcion assign
+ */
+func valueAssigned(idNode int) string {
+	return fmt.Sprintf("%v", nodes[idNode].data.(map[string]interface{})["url"])
 }
